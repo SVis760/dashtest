1af455b6a06e869c5262145272e474171ddb5072
Sablina Vis <101023782+SVis760@users.noreply.github.com>
2025-02-09 20:53:06 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.io as pio
+
+app = dash.Dash(__name__)
+
+# Define your layout and callbacks here
+app.layout = html.Div([
+    dcc.Graph(id='example-graph', figure={'data': [{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Example'}]})
+])
+
+# Export the app to an HTML file
+pio.write_html(app.to_html(), file='index.html')
